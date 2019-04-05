196148d1e45db26abc3b465fab31387f62081401
liang ma <liangma@liangdeMacBook-Pro.local>
2019-04-05 23:46:08 +0800

    创建视图组件，单独做视图展示，即动态组件创建

--- src/app/pages/development-page/comp-view/comp-view.component.ts
+++ src/app/pages/development-page/comp-view/comp-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit, ComponentFactoryResolver, ElementRef, Input, ViewChild } from '@angular/core';
+import { ViewContainRefHostDirective } from '../../../communal/directive/view-contain-ref-host.directive';
 
 @Component({
   selector: 'app-comp-view',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comp-view.component.scss']
 })
 export class CompViewComponent implements OnInit {
-
+  @ViewChild(ViewContainRefHostDirective) viewContRef: ViewContainRefHostDirective;
+  currentViewContRef: any; //当前组件实例
+  
   constructor() { }
 
   ngOnInit() {
+    this.currentViewContRef = this.viewContRef.viewContainerRef;
   }
 
 }
