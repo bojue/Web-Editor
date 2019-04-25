10c79fb4f087a4e1cbbbbd748bdb896598f43216
bojue <lansemaikefeng@126.com>
2019-04-25 16:36:30 +0800

    添加图层

--- src/app/component/dev/settings/style/z-index/z-index.component.ts
+++ src/app/component/dev/settings/style/z-index/z-index.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { StyleBasicComponent } from 'src/app/component/basic/style-basic/style-basic.component';
 
 @Component({
   selector: 'app-z-index',
   templateUrl: './z-index.component.html',
   styleUrls: ['./z-index.component.scss']
 })
-export class ZIndexComponent implements OnInit {
-
-  constructor() { }
+export class ZIndexComponent extends StyleBasicComponent implements OnInit {
+  zIndexS:any[];
+  constructor() { 
+    super()
+  }
 
   ngOnInit() {
+    this.zIndexS = [
+      1,2,3,4,5,6,7,8,9,10
+    ]
+  }
+
+  setStyle(param, val) {
+    this.style[param] = val;
+    console.log(this.style)
   }
 
 }
