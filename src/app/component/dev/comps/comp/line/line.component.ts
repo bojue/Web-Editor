365340c26aaac03c70a713d4de4366c6ded7e52e
liang ma <liangma@liangdeMacBook-Pro.local>
2019-05-01 23:45:47 +0800

    直线组件

--- src/app/component/dev/comps/comp/line/line.component.ts
+++ src/app/component/dev/comps/comp/line/line.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef } from "@angular/core";
 import { BasicComponent } from "src/app/component/basic/basic/basic.component";
 import { SettingObjComponent } from "src/app/module/setting-object.component";
 
@@ -8,11 +8,27 @@ import { SettingObjComponent } from "src/app/module/setting-object.component";
   styleUrls: ['./line.component.scss']
 })
 export class LineComponent extends BasicComponent implements OnInit, SettingObjComponent{
-  constructor() {
+  constructor(
+    private element: ElementRef
+  ) {
     super();
   }
 
   ngOnInit() {
     this.initData();
+    this.getLine();
+  }
+
+  getLine() {
+    let _qEle = "#canvas-line";
+    let line_canvas = this.element.nativeElement.querySelector(_qEle);
+    var ctx = line_canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.lineWidth=1;
+    ctx.lineCap="butt";
+    ctx.moveTo(20,20);
+    ctx.lineTo(200,20);
+    ctx.stroke();
   }
 }
+ 
