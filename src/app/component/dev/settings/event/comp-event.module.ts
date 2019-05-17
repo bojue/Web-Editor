4097037cc75c3a7940f5d163b8118e906088d422
maliang <maliang@example.com>
2019-05-17 15:25:02 +0800

    update

--- src/app/component/dev/settings/event/comp-event.module.ts
+++ src/app/component/dev/settings/event/comp-event.module.ts
@@ -1,17 +1,11 @@
-import { ClickEventComponent } from "./click-event/click-event.component";
-import { DbclickEventComponent } from "./dbclick-event/dbclick-event.component";
 import { RouteEventComponent } from "./route-event/route-event.component";
-import { EventListComponent } from "./event-list/event-list.component";
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { CompSettingBasicModule } from "src/app/component/basic/comp-setting-basic.module";
 
 const styleComp = [
-    ClickEventComponent,
-    DbclickEventComponent,
     RouteEventComponent,
-    EventListComponent,
 ]
 
 @NgModule({
