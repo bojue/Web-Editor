d8a3ab9ab59187584b77bd4cc40396ae2a594db7
maliang <maliang@example.com>
2019-05-17 15:11:53 +0800

    '组件设置样式重构'

--- src/app/component/dev/settings/style/opacity/opacity.component.ts
+++ src/app/component/dev/settings/style/opacity/opacity.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { StyleBasicComponent } from 'src/app/component/basic/style-basic/style-basic.component';
 
 @Component({
   selector: 'app-opacity',
   templateUrl: './opacity.component.html',
   styleUrls: ['./opacity.component.scss']
 })
-export class OpacityComponent implements OnInit {
-
-  constructor() { }
-
+export class OpacityComponent extends StyleBasicComponent implements OnInit {
+  fontAlign: any[];
+  constructor() { 
+    super()
+  }
+  
   ngOnInit() {
   }
 
