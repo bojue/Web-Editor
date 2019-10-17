dadfb7c3d4e27d6b2bdb729da9b7ea0697fd4158
bojue <lansemaikefeng@126.com>
2019-10-17 23:45:02 +0800

    切换组态

--- src/app/pages/develop/comp-page/comp-page.component.ts
+++ src/app/pages/develop/comp-page/comp-page.component.ts
@@ -20,9 +20,8 @@ export class CompPageComponent implements OnInit {
   }
 
   //选择当前可编辑页面，传递页面组件列表当父类容器渲染
-  selectNodeEvent(event) {
-    let currenPage = event && event['node'] && event['node']['node'] || {};
-    this.selCurrentPage.emit(currenPage);
+  changeComponent(item) {
+    this.selCurrentPage.emit(item);
   }
 
 }
