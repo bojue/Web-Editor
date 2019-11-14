59eaf8d539d39fb709618614e98a306d581df6f3
bojue <lansemaikefeng@126.com>
2019-11-14 23:31:21 +0800

    请求骨架

--- src/app/core/provider/baseHttp/base-http.service.ts
+++ src/app/core/provider/baseHttp/base-http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from 'selenium-webdriver/http';
 import * as _ from 'lodash'; 
 import { URL_STRING } from './InjectUrlString';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +9,38 @@ import { URL_STRING } from './InjectUrlString';
 export class BaseHttpService {
 
   constructor(
-    public httpClient: HttpClient,
+    public https: HttpClient,
     @Inject(URL_STRING) public baseURL:string
   ) { }
+
+  header(params?:any, url?:string) {
+    return this.https.head(url);
+  }
 
   get(params:any, url?:string) {
     let _paramsIsObjectBool = _.isObject(params);
     let _url = url || this.baseURL;
+    
+    return this.https.get(url);
+  }
+
+  getAll(url?:string) {
+    return this.https.get('url');
+  }
+
+  create(params:any, url?:string,  header?:any) {
+    return this.https.put(url, params);
+  }
+
+  update(params:any, url?:string, header?:any) {
+    return this.https.post(url, params);
+  }
+
+  delete(params?:any, url?:string,header?:any) {
+    return this.https.delete(url)
+  }
+
+  deleteObject(params?:any, url?:string, header?:any) {
+    return this.https.delete(url)
   }
 }
