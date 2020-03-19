1a05c7587bbd0a27ad5289d7bd3272028f0d1f9e
bojue <lansemaikefeng@126.com>
2020-03-19 21:45:01 +0800

    update sweet

---
+++ src/app/core/provider/toaster/sweetalert.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import Swal from "sweetalert2";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SweetalertService {
+
+  constructor() { }
+
+  deleteAlert(conent?:any) {
+    
+  }
+}
